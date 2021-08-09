fa020ebd8c82de71ed03ba9bcfed4d8fa79e4647
Foorack <max@foorack.com>
2021-08-09 23:45:04 +0200

    test(friends): start working on friends testing

--- test/test.js
+++ test/test.js
@@ -8,6 +8,8 @@ chai.use(chaiResponseValidator(path.resolve("dist/openapi.yaml")));
 
 // Import APIs
 const auth = require("./api/auth");
+const files = require("./api/files");
+const friends = require("./api/friends");
 const users = require("./api/users");
 const system = require("./api/system");
 const worlds = require("./api/worlds");
@@ -20,8 +22,17 @@ describe("Testing System API", () => {
     system();
 });
 
+describe("Testing Files API", () => {
+    // "You can't upload files right now"
+    // files();
+});
+
 describe("Testing Users API", () => {
     users();
+});
+
+describe("Testing Friends API", () => {
+    friends();
 });
 
 describe("Testing Worlds API", () => {
