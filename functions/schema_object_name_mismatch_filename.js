67b90254035b5c1b4dac7bd7d21bf5fbffdd7927
Foorack / Max Faxälv <max@foorack.com>
2022-12-04 17:42:26 +0100

    WIP on Linting

--- functions/schema_object_name_mismatch_filename.js
+++ functions/schema_object_name_mismatch_filename.js
@@ -18,32 +18,29 @@ export default createRulesetFunction(
   function schema_object_name_mismatch_filename(input, options, context) {
     const results = [];
     const { title } = input;
-    let prefix;
-    if (context.path[3] === "requestBody" && context.path.length === 7) {
-      prefix = "requests";
-    } else if (context.path[3] === "response" && context.path.length === 7) {
-      prefix = "responses";
-    } else {
-      prefix = "schemas";
+
+    if (title === "APIConfig") {
+      console.log(context);
+      let abc = context.documentInventory.referencedDocuments['/home/foorack/Documents/git/specification/openapi/components/schemas/APIConfig.yaml'];
+      console.log(abc);
+      // console.log(JSON.stringify(abc.data) === JSON.stringify(input));
+      console.log(JSON.stringify(abc.data));
+      console.log(JSON.stringify(input));
     }
 
-    const documents = Object.keys(
-      context.documentInventory.referencedDocuments
-    );
+    // let match = false;
+    // for (const doc of documents) {
+    //   // console.log(doc);
+    //   if (doc.endsWith(`${prefix}/${title}.yaml`)) {
+    //     match = true;
+    //   }
+    // }
 
-    let match = false;
-    for (const doc of documents) {
-      // console.log(doc);
-      if (doc.endsWith(`${prefix}/${title}.yaml`)) {
-        match = true;
-      }
-    }
-
-    if (!match) {
-      results.push({
-        message: `Schema title "${title}" does not match filename, expected ${prefix}/${title}.yaml: ${context.path}`,
-      });
-    }
+    // if (!match) {
+    //   results.push({
+    //     message: `Schema title "${title}" does not match filename, expected ${prefix}/${title}.yaml: ${context.path}`,
+    //   });
+    // }
 
     return results;
   }
