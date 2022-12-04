26076f366271197a2e3fb477d427da065da0d83a
Foorack / Max Faxälv <max@foorack.com>
2022-12-04 22:09:16 +0100

    Implement remaining Group API endpoints (#208)
    * Update paths.yaml
    
    * Sort Group endpoints
    
    * List Announcements
    
    * GroupRequestAction
    
    * Create Announcement
    
    * GroupAlreadyMemberError
    
    * Does not return list of announcements
    
    * Announcements
    
    * Clear Announcement
    
    * Started Audit logs
    
    * Member Endpoint
    
    * Group Member Roles Endpoint
    
    * Update CreateGroupAnnouncementRequest.yaml
    
    * Add endpoints to path.yaml
    
    * GroupMemberStatus
    
    * Update UpdateGroupMemberRequest.yaml
    
    * Group Kick and Ban endpoints
    
    * Fix Group Object
    
    * GroupRoleIDList
    
    * Group Audit Log
    
    * Group List Available Permissions
    
    * WIP on Linting
    
    * Remove broken lint rule
    
    * Gallery API
    
    * Cursed stuff all the way
    
    * Update UpdateGroupGalleryRequest.yaml
    
    * extra fiels to create and update group
    
    * GroupGalleryImageDeleteForbiddenError
    
    * Update paths.yaml
    
    Co-authored-by: Miner28 <suchys.jakub@gmail.com>

--- functions/print.js
+++
@@ -1,9 +0,0 @@
-export default (input) => {
-  // Debug: Print the type and value
-  console.log(typeof input, input);
-  return [
-    {
-      message: "Value type: " + typeof input + ", value: " + JSON.stringify(input),
-    },
-  ];
-};
