342d2f6bf79c321c0a7aed57ee0942a8f8215c23
Aries <me@ariesclark.com>
2025-11-26 14:39:50 -0700

    chore: lint autofix

--- spectral/functions/casing.js
+++ spectral/functions/casing.js
@@ -4,28 +4,28 @@ import { createRulesetFunction } from "@stoplight/spectral-core";
 import { camelCase, pascalCase } from "change-case";
 
 const types = {
-  camelCase,
-  pascalCase
+	camelCase,
+	pascalCase
 };
 
 export const casing = createRulesetFunction({
-  input: {
-    type: "string",
-  },
-  options: {
-    type: "object",
-    properties: {
-      type: {
-        enum: Object.keys(types),
-      }
-    },
-  }
+	input: {
+		type: "string",
+	},
+	options: {
+		type: "object",
+		properties: {
+			type: {
+				enum: Object.keys(types),
+			}
+		},
+	}
 }, (value, { type }) => {
-  // @ts-expect-error
-  const suggestedName = types[type](value);
-  if (value === suggestedName) return [];
+	// @ts-expect-error
+	const suggestedName = types[type](value);
+	if (value === suggestedName) return [];
 
-  return [{
-    message: `Consider renaming to "${suggestedName}".`,
-  }];
+	return [{
+		message: `Consider renaming to "${suggestedName}".`,
+	}];
 });
