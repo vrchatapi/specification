37c59fe52d876e7e40f2988ddc2839958226902e
Aries <me@ariesclark.com>
2025-11-26 16:27:59 -0700

    chore(lint): sort keys & arrays

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,120 @@ export default configure({
 	rules: {
 		"unicorn/prevent-abbreviations": "off",
 	}
+}, {
+	files: [
+		"openapi/**/*.yaml",
+	],
+	rules: {
+		"yaml/sort-keys": [
+			"error",
+			{
+				pathPattern: ".*",
+				hasProperties: [
+					"openapi"
+				],
+				order: [
+					"openapi",
+					"info",
+					"servers",
+					"paths",
+					"components",
+					"security",
+					"tags",
+					"externalDocs"
+				]
+			},
+			{
+				pathPattern: ".*",
+				hasProperties: [
+					"operationId"
+				],
+				order: [
+					"operationId",
+					"deprecated",
+					"summary",
+					"description",
+					"tags",
+					"parameters",
+					"requestBody",
+					"security",
+					"responses",
+				]
+			},
+			{
+				pathPattern: "^\.*$",
+				hasProperties: [
+					"parameters"
+				],
+				order: [
+					"parameters",
+					"get",
+					"put",
+					"post",
+					"delete",
+					"options",
+					"head",
+					"patch",
+					"trace",
+					{
+						order: {
+							type: "asc"
+						}
+					}
+				]
+			},
+			{
+				pathPattern: "^properties$",
+				order: [
+					{
+						order: {
+							type: "asc"
+						}
+					}
+				]
+			},
+			{
+				pathPattern: ".*",
+				order: [
+					"id",
+					"title",
+					"name",
+					"deprecated",
+					"$ref",
+					"type",
+					"description",
+					"nullable",
+					"additionalProperties",
+					"properties",
+					"required",
+					"enum",
+					"default",
+					"example",
+					{
+						order: {
+							type: "asc"
+						}
+					}
+				]
+			}
+		],
+		"yaml/sort-sequence-values": [
+			"error",
+			{
+				pathPattern: ".*",
+				order: [
+					{
+						order: {
+							type: "asc"
+						}
+					}
+				]
+			}
+		]
+	}
+}, {
+	files: ["openapi/openapi.yaml"],
+	rules: {
+		"yaml/sort-keys": "off"
+	}
 });
