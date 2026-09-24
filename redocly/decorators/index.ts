30b33a1b57efabfa7159af547e68ec5c245359ac
Aries Clark <me@ariesclark.com>
2026-09-24 14:01:16 -0400

    feat: point AI agents to the SDKs and to contributing upstream

--- redocly/decorators/index.ts
+++ redocly/decorators/index.ts
@@ -1,7 +1,9 @@
 import { closeSchemas } from "./close-schemas.ts";
+import { infoExtensionOverride } from "./info-extension-override.ts";
 import { removeXDeleted } from "./remove-x-deleted.ts";
 
 export const decorators = {
 	"close-schemas": closeSchemas,
+	"info-extension-override": infoExtensionOverride,
 	"remove-x-deleted": removeXDeleted
 };
